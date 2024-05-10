c947813e155613ed5c1c96707be4030e4087e8c9
VictoriaSCorreia <104169748+VictoriaSCorreia@users.noreply.github.com>
2024-05-10 17:46:28 -0300

    First commit

---
+++ chatbot.py
@@ -0,0 +1,43 @@
+""" !pip install -q -U google-generativeai """
+
+import google.generativeai as genai 
+
+GOOGLE_API_KEY="" 
+genai.configure(api_key=GOOGLE_API_KEY) 
+''' genai.configure(api_key="") '''
+
+# Verifying Existing Genai Models
+for m in genai.list_models():
+  if 'generateContent' in m.supported_generation_methods:
+    print(m.name)
+
+# Configuration
+generation_config = {
+  "temperature": 1,
+  "top_p": 0.95,
+  "top_k": 0,
+  "max_output_tokens": 8192,
+}
+safety_settings = {
+    "HARASSMENT": "BLOCK_NONE",
+    "DANGEROUS": "BLOCK_NONE",
+    "SEXUAL": "BLOCK_NONE",
+    "HATE": "BLOCK_NONE"
+}
+
+# Creating a new model
+model = genai.GenerativeModel(model_name="gemini-1.0-pro",
+                              generation_config=generation_config,
+                              safety_settings=safety_settings)
+
+
+''' response = model.generate_content("Write a story about a magic backpack.")
+print(response.text) '''
+
+chat = model.start_chat(history=[]) # Starting a chat
+while True:
+  prompt = input("Esperando prompt: ") 
+  response = chat.send_message(prompt)
+  print(response.text)
+  if prompt == "fim":
+    break

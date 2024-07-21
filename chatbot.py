6a35b09e2deb766f18dbacf5aa5fd9b2445e64cd
Victoria Correia <104169748+VictoriaSCorreia@users.noreply.github.com>
2024-07-21 20:32:02 -0300

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -31,7 +31,7 @@ model = genai.GenerativeModel(model_name="gemini-1.0-pro",
                               generation_config=generation_config,
                               safety_settings=safety_settings)
 
-''' response = model.generate_content("Write a story about a dark road.")
+''' response = model.generate_content("Write a story about a witch hat.")
 print(response.text) '''
 
 chat = model.start_chat(history=[]) # Starting a chat (history will allow it to "remember" the previous questions and the context)
